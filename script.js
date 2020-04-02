3f04bbf80cab559cf0bc2f49cf853d8f457a2cc8
Deborah Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2020-04-02 12:31:50 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+//Set my age
+let myAge = 23;
+
+//A dog's early years
+let earlyYears = 2;
+
+let dogYears = (earlyYears * 10.5);
+
+//A dog's later years
+let laterYears = myAge - 2;
+
+//My age in later dog years
+var newAge = laterYears * 4; 
+
+console.log(`If I were a dog, I would be ${newAge} years old.`);
+
+//Calculate age in dog years
+let myAgeInDogYears = newAge + dogYears;
+
+//Putting my name in a string method which retuns it with lowercase letters
+let myName = "Debbie".toLowerCase();
+
+//Show my age in dog years
+console.log(`My name is ${myName}. I am ${myAge} in human years, which is ${myAgeInDogYears} in dog years.`);
+
+
